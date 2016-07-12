30ad62a1c628b3c9e0efd9c8f4f2e7850426d4ef
Mao <604138548@qq.com>
2016-07-12 15:28:58 +0800

    新增项目

--- WuTuojia/WuTuojia/Classes/Main(基础)/AppDelegate.m
+++ WuTuojia/WuTuojia/Classes/Main(基础)/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[[HomeViewController alloc]init]];
     [self.window makeKeyAndVisible];
